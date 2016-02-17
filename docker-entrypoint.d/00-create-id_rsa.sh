c4046735ebc39b67d4b92c3cdc4eb7aea7549c72
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-17 11:07:11 +0100

    Revert "Use puppet RSA key for SSH"
    This reverts commit beebe77c7ea10864835e283a82f89e79258025a1.

---
+++ docker-entrypoint.d/00-create-id_rsa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if test -n "${RSA_PRIVATE_KEY}"; then
+  echo -e $(echo $RSA_PRIVATE_KEY | sed -e 's/ /\\n/g' \
+    -e 's/-----BEGIN\\nRSA\\nPRIVATE\\nKEY-----/-----BEGIN RSA PRIVATE KEY-----/' \
+    -e 's/-----END\\nRSA\\nPRIVATE\\nKEY-----/-----END RSA PRIVATE KEY-----/') > /root/.ssh/id_rsa
+  chmod 0600 /root/.ssh/id_rsa
+fi
--- docker-entrypoint.d/02-create-id_rsa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cp /etc/puppetlabs/puppet/ssl/private_keys/$(hostname -f).pem /root/.ssh/id_rsa
