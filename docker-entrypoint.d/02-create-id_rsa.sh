db3f3e4c644bc11829ebfb5e5985b0593bcf9e4b
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-17 11:07:08 +0100

    Revert "Use container uuid as certname"
    This reverts commit bae58e13c5a07efb99383a6e3247025fe850ee35.

--- docker-entrypoint.d/02-create-id_rsa.sh
+++ docker-entrypoint.d/02-create-id_rsa.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-if getent hosts rancher-metadata > /dev/null ; then
-  cp /etc/puppetlabs/puppet/ssl/private_keys/$(curl http://rancher-metadata/latest/self/container/uuid 2> /dev/null).pem /root/.ssh/id_rsa
-fi
+cp /etc/puppetlabs/puppet/ssl/private_keys/$(hostname -f).pem /root/.ssh/id_rsa
