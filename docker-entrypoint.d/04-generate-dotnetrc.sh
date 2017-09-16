e2b9173006f437d4d63aa53c8c8329248ca2ca50
Ezra Forbus <eforbus@cisco.com>
2017-09-15 21:06:01 -0500

    Added GITHUB_URL and GITHUB_HOST vars for enterprise github

--- docker-entrypoint.d/04-generate-dotnetrc.sh
+++ docker-entrypoint.d/04-generate-dotnetrc.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+# allow to set alternative github api url
+# for enterprise github installs
+GITHUB_HOST="${GITHUB_HOST:-api.github.com}"
+GITHUB_URL="${GITHUB_URL:-https://${GITHUB_HOST}}"
+
 # Retrieve GITHUB_USER from GITHUB_TOKEN
-GITHUB_USER=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/user | sed -n '/ *"login": "\(.*\)",$/ s//\1/p')
+GITHUB_USER=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" ${GITHUB_URL}/user | sed -n '/ *"login": "\(.*\)",$/ s//\1/p')
 
 if test -n "${GITHUB_USER}" && test -n "${GITHUB_TOKEN}"; then
   cat << EOF > /root/.netrc
@@ -10,7 +15,7 @@ machine github.com
 login ${GITHUB_USER}
 password ${GITHUB_TOKEN}
 
-machine api.github.com
+machine ${GITHUB_HOST}
 login ${GITHUB_USER}
 password ${GITHUB_TOKEN}
 EOF
